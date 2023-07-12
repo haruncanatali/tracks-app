4a2a2266e8c0b5f7c789b8116bcd0d49cf60d938
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2023-07-12 13:24:46 +0300

    Project-Skeleton

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 import requests
 import random
 import json
+import datetime
 
 app = Flask(__name__)
+
+expires = datetime.datetime.now()
+access_key = ""
 
 
 @app.route('/home')
@@ -14,7 +18,19 @@ def index():
 @app.route('/tracks/<genre>')
 def get_tracks(genre):
     artist = get_artist_from_json_file(genre)
+
+    if artist is None:
+        return []
+
     return artist
+
+
+def get_access_key():
+    if datetime.datetime.now() < expires and access_key is not None and len(access_key) != 0:
+        return access_key
+    else:
+        return ""
+    return ""
 
 
 def get_artist_from_json_file(genre):
